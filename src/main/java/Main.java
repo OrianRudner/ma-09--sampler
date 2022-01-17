3546898bbcba9d8daeb6c4e91890564a7063a8b8
OrianRudner <orian.rudner@gmail.com>
2022-01-17 14:51:50 +0200

    delete unnecessary code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,3 @@
-import Extract.ExtractDataFromCSV;
-import Models.MadaSample;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.csv.CSVRecord;
-import parsers.ParseCSVToMadaSample;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
