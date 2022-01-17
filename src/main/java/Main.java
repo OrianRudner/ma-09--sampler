2d7cd12139c4bed3eb1640ecf7c16815737beafb
OrianRudner <orian.rudner@gmail.com>
2022-01-17 14:47:23 +0200

    part 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
         ETLManager manager = new ETLManager();
         manager.MadaRecordsToJson("C:\\Users\\orian\\Desktop\\curseProject\\sampler\\src\\main\\resources\\MadaReports.csv",
-                "C:\\Users\\orian\\Desktop\\curseProject\\sampler files\\mada");
+                "C:\\Users\\orian\\Desktop\\curseProject\\sampler files\\mada_reports\\mada");
 
     }
 }
